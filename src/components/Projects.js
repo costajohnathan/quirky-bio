f71975108c7f8ee1b3b46156701b3158582adb30
Johnathan Costa <john@192-168-1-100.tpgi.com.au>
2021-02-20 23:19:12 +1000

    Grammatical update

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,7 @@ const Projects = () => {
     return (
         <div className="Projects">
             <h2>Projects</h2>
-            <li>Self hosting websites from Apache running in a raspberry Pi</li>
+            <li>Self hosting websites from Apache running in a Raspberry Pi</li>
             <li>Developed and deployed <a href="http://www.mahikastore.com.au" target="_blank">MahikaStore.com</a> on AWS' EC2 </li>
             <li>Hosting serverless websites on the Cloud with AWS' Labda</li>
         </div>
