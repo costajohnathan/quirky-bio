9fdf6462280f8f77be08cf09f7bcd8bc077a84e6
Johnathan Costa <john@192-168-1-101.tpgi.com.au>
2021-02-21 22:12:50 +1000

    text-styling

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -14,13 +14,13 @@ const Resume = () => {
             </div> 
             <div className="paragraph">
                 <h2>Projects</h2>
-                <li>React + Amazon Lambda - 2021</li>
+                <li><b>React + Amazon Lambda - 2021</b></li>
                 <p className="lower-case">Digital resume made on React, mainteined with Git, and set up Amazon Lambda to use <a href="https://github.com/costajohnathan/quirky-bio" target="_blank">this GitHub repository</a> to serve as the root folder.</p>
                 <p className="lower-case">Metrics are collected by GoogleAnalytics trough a HTML inline script for future assesment of the website.</p>
-                <li>WordPress + Amazon EC2 - 2020</li> 
+                <li><b>WordPress + Amazon EC2 - 2020</b></li> 
                 <p className="lower-case">Developed and deployed <a href="http://www.mahikastore.com.au" target="_blank">MahikaStore.com</a>. The project was developed in WordPress, since speed was paramount, and a bitnami image was used as host server.</p>
                 <p className="lower-case">Although Stripe & Paypal were successfully activated, the sample order was unsatisfactory, thus, no stock has been purchased since.</p>
-                <li>LAMP Server + Raspberry Pi - 2019</li>
+                <li><b>LAMP Server + Raspberry Pi - 2019</b></li>
                 <p className="lower-case">Self hosted my <a href="https://www.freecodecamp.org/costajohnathan" target="_blank">FreeCodeCamp.com</a>. projects locally. Addressed the lack of static IP with NoIP, and TSL with LetsEncrypt.</p>
             </div>
             <div className="paragraph">
@@ -35,7 +35,7 @@ const Resume = () => {
                 <h2>Skills</h2>
                 <div className="row" >
                     <div className="column">
-                        <ul>Proficient</ul>
+                        <ul><b>Proficient</b></ul>
                         <div className="lower-case">
                             <li>Terminal</li>
                             <li>Python</li>
@@ -45,7 +45,7 @@ const Resume = () => {
                         </div>
                     </div> 
                     <div className="column">
-                        <ul>Familiar</ul>
+                        <ul><b>Familiar</b></ul>
                         <div className="lower-case">
                             <li>Javascript</li>
                             <li>Apache</li>
@@ -59,7 +59,7 @@ const Resume = () => {
             
             <div className="paragraph">
                 <h2>Education</h2>
-                <p>Bachelor of Computer Science</p>
+                <p><b>Bachelor of Computer Science</b></p>
                 <p className="lower-case">The University of Queensland [2020 ~ 2023]</p>
             </div>  
 
