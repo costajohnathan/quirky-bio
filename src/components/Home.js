e6ab79d7f478466c1d6d831600c01ae3e1d19e92
Johnathan Costa <john@192-168-1-101.tpgi.com.au>
2021-02-21 21:17:05 +1000

    deleting-white-spaces

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
     return (
         <div className="centered-container">
             <div className="centered-text">
-                <h1 >Howdy!</h1>
+                <h1>Howdy!</h1>
             </div>
         </div>
     )
