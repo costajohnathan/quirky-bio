d1d21fc7401f4bee786a9317a52a9452fd95c32b
Johnathan Costa <john@192-168-1-101.tpgi.com.au>
2021-02-21 21:10:06 +1000

    adding-react-router

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,7 +5,7 @@ const Resume = () => {
         <div>
             <h1 className="title">Resume</h1>
             <div className="header">
-                <h2>Johnthan Costa</h2>
+                <h2>Johnathan Costa</h2>
                 <div className="lower-case">
                     <p><a href="https://www.linkedin.com/in/johnathan-costa-6087921b5/" target="_blank">Linkedin </a>|
                     <a href="https://github.com/costajohnathan" target="_blank"> GitHub</a></p>
