e29df1864dcf467a027551ce872825692e6cca7d
Johnathan Costa <john@192-168-1-101.tpgi.com.au>
2021-02-21 21:21:02 +1000

    aligning-about-section

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>
-            <h1>Hello World!</h1>
-            <p>I'm Johnathan Costa, Computer Science student based in Brisbane, Australia.</p>
+        <div className="centered-container">
+            <div className="centered-text">
+                <h1>Hello World!</h1>
+                <p>I'm Johnathan Costa, Computer Science student based in Brisbane, Australia.</p>
+            </div>
         </div>
     )
 }
