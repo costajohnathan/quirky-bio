362ee77327d6164553fc0712515750ee467f1cee
Johnathan Costa <john@192-168-1-101.tpgi.com.au>
2021-02-21 13:28:52 +1000

    content filesystem redesign

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import logo from './logo.svg';
 import './index.css';
-import Header from './components/Header';
-import Projects from './components/Projects';
-import Experience from './components/Experience';
-import Skills from './components/Skills';
-import Education from './components/Education';
+import Home from './components/Home';
 import Resume from './components/Resume';
 
 
@@ -14,11 +10,6 @@ function App() {
   return (
     <div className="div">
       <Resume />
-      <Header />
-      <Projects />
-      <Experience />
-      <Skills />
-      <Education />
     </div>
   );
 }
